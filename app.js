cfc2974df5e2f68a21bd0b8dc240859058e01c14
nick <nickxylanmelloria@gmail.com>
2023-10-27 12:13:10 +0800

    can now add books

--- app.js
+++ app.js
@@ -1,51 +1,101 @@
 const domLibrary = document.querySelector('.cards');
+const popup = document.querySelector('.popup');
+const showButton = document.querySelector('.addBook');
+const domAuthor = document.querySelector('#author');
+const domTitle = document.querySelector('#title');
+const domPublished = document.querySelector('#published');
+const domDescription = document.querySelector('#description');
+
+
+// let newBook;
 
 const library = [];
 
-function Book(author, title, published) {
+function Book(author, title, published, description) {
     this.author = author;
     this.title = title;
     this.published = published;
+    this.description = description;
     this.read = false;
 }
 
-function createBook(author, title, published) {
-    return newBook = new Book(author, title, published)
+function createBook() {
+    const newBook = new Book(domAuthor.value, domTitle.value, domPublished.value, domDescription.value);
+    addBookToLibrary(newBook);
+    renderBook(library);
+    console.log(library);
 }
 
-function addBookToLibrary(newBook) {
-    library.push(newBook);
+function addBookToLibrary(book) {
+    library.push(book);
 }
-
-const book1 = createBook('Nick', 'The Cunt', 1980);
-addBookToLibrary(book1);
-
-const book2 = createBook('RJ', 'Skibidi', 1950);
-addBookToLibrary(book2);
-
-// console.log(library);
 
 function renderBook(library) {
-    let counter = 0;
-    for (let i = 1; i <= library.length; i++) {
+    let prev = domLibrary.querySelectorAll("div");
+    prev.forEach((div) => div.remove());
+    for (let i = 0; i <= library.length - 1; i++) {
         const book = document.createElement("div");
-        book.classList.add('bookCard');
+        book.classList.add('cardContainer');
         domLibrary.appendChild(book);
+        //cards
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.id = i;
+        book.appendChild(card);
+        //delete button
+        const deleteButton = document.createElement("button");
+        // deleteButton.innerText("X");
+        deleteButton.textContent = "delete";
+        deleteButton.className = "deleteButton";
+        deleteButton.addEventListener('click', () => {
+            alert(card.id);
+        });
+        card.appendChild(deleteButton);
         //title
         const title = document.createElement("h2");
-        title.innerText = library[counter].title;
-        book.appendChild(title);
+        title.innerText = library[i].title;
+        card.appendChild(title);
         //author
         const author = document.createElement("p");
-        author.innerText = library[counter].author;
-        book.appendChild(author);
+        author.innerText = library[i].author;
+        card.appendChild(author);
         //published
         const published = document.createElement("h3");
-        published.innerText = library[counter].published;
-        book.appendChild(published);
-
-        counter++;
+        published.innerText = library[i].published;
+        card.appendChild(published);
+        //description
+        const description = document.createElement("p");
+        description.innerText = library[i].description;
+        card.appendChild(description);
     }
 }
 
-renderBook(library);
+// const deleteButton = document.querySelector('.deleteButton');
+
+showButton.addEventListener('click', () => {
+    popup.show();
+});
+
+// deleteButton.addEventListener('click', () => {
+//     alert("deleted");
+// });
+
+// function test() {
+//     alert(domAuthor.value);
+// }
+
+// const book1 = createBook('Nick', 'The Cunt', 1980);
+// addBookToLibrary(book1);
+
+// const book2 = createBook('RJ', 'Skibidi', 1950);
+// addBookToLibrary(book2);
+
+// const book3 = createBook('Lopez', 'Jokeron', 1669);
+// addBookToLibrary(book3);
+
+// const book4 = createBook('Zoid', 'Jokeron 2', 1420);
+// addBookToLibrary(book4);
+
+// renderBook(library);
+
+// console.log(library);
